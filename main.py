db207651116892376f8803857e1ee7fa93011a2a
umer <umertaiyab@gmail,com>
2024-10-13 22:01:12 +0500

    Changed data storage model from txt files to json files and added search functionality to the password generator.

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 import tkinter
+from json import JSONDecodeError
 from tkinter import messagebox
 import string
 import random
+import json
 
 password_selection_list = string.ascii_letters + string.digits + string.punctuation
+
+# ---------------------------- SEARCH FUNCTION ------------------------------- #
+def search():
+    website = website_entry.get()
+    with open("data.json", "r") as data_file:
+        try:
+            data = json.load(data_file)
+        except JSONDecodeError:
+            messagebox.showinfo(message="No Password Stored")
+            return
+        else:
+            try:
+                website_data = data[website]
+            except KeyError:
+                messagebox.showinfo(message="Password for this\n"
+                                            "Website is not stored")
+                return
+            else:
+                email, password = website_data.items()
+                messagebox.showinfo(title=f"{website}", message=f"Email: {email[1]}\n"
+                                                                f"Password: {password[1]}")
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
     password_entry.delete(0, tkinter.END)
@@ -18,6 +41,12 @@ def add_password():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "Email": email,
+            "Password": password
+        }
+    }
     if (len(website) == 0) or (len(email) == 0) or (len(password) == 0):
         messagebox.showinfo(message="A field was left empty\n"
                                     "Please fill all the fields")
@@ -29,9 +58,19 @@ def add_password():
                                                        f"Is this Correct")
 
     if save_or_not:
-        with open("data.txt", "a") as file:
-            file.write(f"{website} | {email} | {password}\n")
-            file.close()
+        try:
+            with open("data.json", "r") as data_file:
+                try:
+                    data = json.load(data_file)
+                except JSONDecodeError:
+                    data = {}
+                data.update(new_data)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
 
         website_entry.delete(0, tkinter.END)
         email_entry.delete(0, tkinter.END)
@@ -57,8 +96,8 @@ password_label = tkinter.Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 #ALL the entry boxes
-website_entry = tkinter.Entry(width=44)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = tkinter.Entry(width=26)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 email_entry = tkinter.Entry(width=44)
 email_entry.grid(row=2, column=1, columnspan=2)
@@ -70,5 +109,7 @@ generate_password_button = tkinter.Button(text="Generate Password", command=gene
 generate_password_button.grid(row=3, column=2)
 add_button = tkinter.Button(text="ADD", width=37, command=add_password)
 add_button.grid(row=4, column=1, columnspan=2)
+search_button = tkinter.Button(text="Search", command=search, width=15)
+search_button.grid(row=1, column=2)
 
 window.mainloop()
